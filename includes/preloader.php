ef60805c8735a8803d31f08881d80cd520dd05b0
Ivonne Mtz <54880593+Ivonnemtz96@users.noreply.github.com>
2023-05-19 13:09:58 -0700

    se crean archivos y se suben al servidor

---
+++ includes/preloader.php
@@ -0,0 +1,7 @@
+<div class="preloader">
+    <button class="vs-btn preloaderCls">Cancel Preloader</button>
+    <div class="preloader-inner">
+        <img src="assets/img/logo.png" alt="Haarino" />
+        <span class="loader"></span>
+    </div>
+</div>
