fbecb0d98f6f8ddb62b6076026817477141ae600
Ivonne Mtz <54880593+Ivonnemtz96@users.noreply.github.com>
2023-05-19 14:24:17 -0700

    scripts con ruta absoluta

--- includes/scripts.php
+++ includes/scripts.php
@@ -1,5 +1,5 @@
-<script src="assets/js/vendor/jquery.min.js"></script>
-<script src="assets/js/app.min.js"></script>
-<script src="assets/js/vscustom-carousel.min.js"></script>
-<script src="assets/js/ajax-mail.js"></script>
-<script src="assets/js/main.js"></script>
+<script src="/assets/js/vendor/jquery.min.js"></script>
+<script src="/assets/js/app.min.js"></script>
+<script src="/assets/js/vscustom-carousel.min.js"></script>
+<script src="/assets/js/ajax-mail.js"></script>
+<script src="/assets/js/main.js"></script>
