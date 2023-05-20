831e597533c6fd3fe951ef9bdeab2dcfc3a40b8d
Ivonne Mtz <54880593+Ivonnemtz96@users.noreply.github.com>
2023-05-19 17:00:51 -0700

    Contacto ok

---
+++ modules/contacto.php
@@ -0,0 +1,220 @@
+<div class="breadcumb-wrapper" data-bg-src="/assets/img/breadcumb/breadcumb-1.jpg">
+    <div class="container z-index-common">
+        <div class="breadcumb-content text-center">
+            <h1 class="breadcumb-title">Contáctanos</h1>
+            <div class="breadcumb-menu-wrap">
+                <ul class="breadcumb-menu">
+                    <li><a href="/">Inicio</a></li>
+                    <li class="active">Contacto</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</div>
+<section class="vs-contact-wrapper space-top space-negative-bottom">
+    <div class="container">
+        <div class="title-area text-center">
+            <div class="sec-icon">
+                <img src="/assets/img/icon/sec-icon.png" alt="Section Title" />
+            </div>
+            <h2 class="sec-title mb-1">Consigue tu próximo look</h2>
+            <p class="sec-desc">
+                Póngase en contacto con nosotros en línea, nos pondremos en contacto lo antes posible.
+            </p>
+        </div>
+        <div class="row gx-30">
+            <div class="col-lg-6 mb-30">
+                <form action="https://html.vecurosoft.com/haarino/demo/mail.php" method="POST" class="ajax-contact">
+                    <div class="row gx-20">
+                        <div class="form-group col-12">
+                            <input type="text" class="form-control style3" name="name" id="name"
+                                placeholder="Nombre *" />
+                        </div>
+                        <div class="form-group col-12">
+                            <input type="email" class="form-control style3" name="email" id="email"
+                                placeholder="Correo *" />
+                        </div>
+                        <div class="form-group col-12">
+                            <input type="number" class="form-control style3" name="number" id="number"
+                                placeholder="Teléfono" />
+                        </div>
+                        <div class="form-group col-12">
+                            <input type="text" class="form-control style3" name="number" id="number"
+                                placeholder="Asunto" />
+                        </div>
+                        <!-- <div class="form-group col-12">
+                            <select name="subject" id="subject" class="form-select style3">
+                                <option value="" disabled="disabled" selected="selected" hidden>
+                                    Service *
+                                </option>
+                                <option value="hair Cut">Hair Cut</option>
+                                <option value="Hair clean">Hair Clean</option>
+                                <option value="Head Massage">Head Massage</option>
+                                <option value="Classic Style">Classic Style</option>
+                            </select>
+                        </div> -->
+                        <div class="form-group col-12">
+                            <textarea name="message" id="message" cols="30" rows="3" class="form-control style3"
+                                placeholder="Message *"></textarea>
+                        </div>
+                        <div class="form-btn col-12">
+                            <button class="vs-btn">Enviar</button>
+                        </div>
+                    </div>
+                    <p class="form-messages mb-0 mt-3"></p>
+                </form>
+            </div>
+            <div class="col-lg-6 mb-30">
+                <div class="vs-info-table">
+                    <table>
+                        <tbody>
+                            <tr>
+                                <td>Dirección:</td>
+                                <td>Blvd. Los Agaves S/N, Monte Real, 23447 San José del Cabo</td>
+                            </tr>
+                            <tr>
+                                <td>Teléfono:</td>
+                                <td>
+                                    <a href="tel:+526242125556" class="text-reset d-inline">624 212 5556</a>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>Email:</td>
+                                <td>
+                                    <a href="mailto:info@fridaysbeautyroom.com"
+                                        class="text-reset d-inline">info@fridaysbeautyroom.com</a>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>Horarios:</td>
+                                <td>Lun-Sab: 9am - 8pm <br> Dom: Cerrados</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<div class="vs-map-wrapper space-bottom">
+    <div class="container">
+        <div class="google-map ratio ratio-16x9">
+            <iframe
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d14681.324882289739!2d-109.7194289698374!3d23.084967908742318!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x86af5a8921359119%3A0xc614cd5741511979!2sFriday&#39;s%20Beauty%20Room!5e0!3m2!1ses-419!2smx!4v1684540770638!5m2!1ses-419!2smx"
+                width="800" height="565" style="border:0;" allowfullscreen="" loading="lazy"
+                referrerpolicy="no-referrer-when-downgrade"></iframe>
+        </div>
+    </div>
+</div>
+<section class="vs-testimonial-wrapper space-top-md-none space">
+    <div class="container position-relative">
+        <div class="avater-fly vs-carousel d-none d-lg-block" id="avaterfly" data-asnavfor="#testimonialslide1"
+            data-slide-show="5" data-centermode="true" data-variable-width="true">
+            <div class="avater">
+                <img src="/assets/img/avater/avater-1-1.png" alt="Testimonial Image" />
+            </div>
+            <div class="avater">
+                <img src="/assets/img/avater/avater-1-3.png" alt="Testimonial Image" />
+            </div>
+            <div class="avater">
+                <img src="/assets/img/avater/avater-1-2.png" alt="Testimonial Image" />
+            </div>
+            <div class="avater">
+                <img src="/assets/img/avater/avater-1-5.png" alt="Testimonial Image" />
+            </div>
+            <div class="avater">
+                <img src="/assets/img/avater/avater-1-4.png" alt="Testimonial Image" />
+            </div>
+        </div>
+        <div class="row justify-content-center">
+            <div class="col-lg-9 col-xl-7">
+                <div class="vs-carousel" id="testimonialslide1" data-asnavfor="#avaterfly" data-fade="true"
+                    data-slide-show="1" data-md-slide-show="1">
+                    <div class="vs-testimonial">
+                        <span class="quote-icon"><img src="/assets/img/icon/quote.png" alt="Quote Icon" /></span>
+                        <div class="testimonial-desc">
+                            <p>
+                                The stylists at our beauty salon are Japanese beauticians
+                                that have extensive experience working in Japan and are
+                                highly skilled.One of these beauticaians will be appointed
+                                as the stylist.
+                            </p>
+                        </div>
+                        <div class="author">
+                            <h3 class="author-name">Martin Truong</h3>
+                            <span class="author-degi">Eikonic House of Barbers</span>
+                        </div>
+                    </div>
+                    <div class="vs-testimonial">
+                        <span class="quote-icon"><img src="/assets/img/icon/quote.png" alt="Quote Icon" /></span>
+                        <div class="testimonial-desc">
+                            <p>
+                                Lorem ipsum is placeholder text commonly used in the
+                                graphic, print, and publishing industries for previewing
+                                layouts and visual mockups non proident, sunt in culpa qui
+                                officia deserunt mollit anim
+                            </p>
+                        </div>
+                        <div class="author">
+                            <h3 class="author-name">William Harry</h3>
+                            <span class="author-degi">Manager Of Commerce</span>
+                        </div>
+                    </div>
+                    <div class="vs-testimonial">
+                        <span class="quote-icon"><img src="/assets/img/icon/quote.png" alt="Quote Icon" /></span>
+                        <div class="testimonial-desc">
+                            <p>
+                                The purpose of lorem ipsum is to create a natural looking
+                                block of text (sentence, paragraph, page, etc.) that doesn't
+                                distract from the layout. A practice not without
+                                controversy, layi
+                            </p>
+                        </div>
+                        <div class="author">
+                            <h3 class="author-name">Thomas Oliver</h3>
+                            <span class="author-degi">Top Ranked Player</span>
+                        </div>
+                    </div>
+                    <div class="vs-testimonial">
+                        <span class="quote-icon"><img src="/assets/img/icon/quote.png" alt="Quote Icon" /></span>
+                        <div class="testimonial-desc">
+                            <p>
+                                The passage experienced a surge in popularity during the
+                                1960s when Letraset used it on their dry-transfer sheets,
+                                and again during the 90s as desktop publishers bundled the
+                                text with their software
+                            </p>
+                        </div>
+                        <div class="author">
+                            <h3 class="author-name">Alexander Alfie</h3>
+                            <span class="author-degi">Actor Of KGF, Germany</span>
+                        </div>
+                    </div>
+                    <div class="vs-testimonial">
+                        <span class="quote-icon"><img src="/assets/img/icon/quote.png" alt="Quote Icon" /></span>
+                        <div class="testimonial-desc">
+                            <p>
+                                Until recently, the prevailing view assumed lorem ipsum was
+                                born as a nonsense text. “It's not Latin, though it looks
+                                like it, and it actually says nothing,” Before & After
+                                magazine t was piqued by
+                            </p>
+                        </div>
+                        <div class="author">
+                            <h3 class="author-name">Matthew Lewis</h3>
+                            <span class="author-degi">Business man of BFG</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="testimonial-arrow d-flex justify-content-center gap-4">
+            <button data-slick-prev="#testimonialslide1" class="slick-arrow slick-prev">
+                <span class="long-arrow"></span>
+            </button>
+            <button data-slick-next="#testimonialslide1" class="slick-arrow slick-next">
+                <span class="long-arrow"></span>
+            </button>
+        </div>
+    </div>
+</section>
